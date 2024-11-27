db03ddf34781f01fd693ef6932311afe73aaa54b
Aiden Brown <aiden-brown4@outlook.com>
2024-11-27 22:17:00 +1030

    Using debugger I can confirm this code works as it should and correctly adds 1 and 2 together to get 3. Using this commit as a fall back point for when I mess with it a bit more in an attempt to get it to print to consol e

---
+++ calculator.asm
@@ -0,0 +1,26 @@
+section .data
+    ;msg1 db "What is the first number?", 0xa         ; Creates a string with the message "Hello, World!"
+    ;len1 equ $ -msg1                             ; Calculates the length of the string
+                                                ; The $ is a pointer and equ is an assignment operator
+    ;msg2 db "What is the second number?", 0xa
+    ;len2 equ $ -msg2
+
+    num1 dd 1                   ; Creates an int with number 1
+    num2 dd 2                   ; Creates an int with number 2
+    ;res dd 0
+
+section .text
+global _start                           ; Tells the linker that the entry point is the start label
+_start:                                 ; starts the program
+    mov al, [num1]                      ; mov puts the num1 variable in al register
+    mov bl, [num2]                      ; add num2 to the al register
+    add al, bl                         ; add makes the al register store the result with res
+    ;mov al, 0x30                       ; Should translate it into ascii to be printed
+    
+    mov ebx, 1                          ; Set File descriptor
+
+    mov eax, 4                          ; syscall number for sys_write
+    int 0x80                            ; Call kernel
+
+    mov eax, 1                          ; System call for exit
+    int 0x80                            ; call kernel
